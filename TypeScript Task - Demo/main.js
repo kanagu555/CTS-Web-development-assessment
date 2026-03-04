f2afb3e30f0b25bca2cc3d6625dcbd73e38eb90f
K, Kanagaraj (kk1569) <kk1569@att.com>
2026-03-04 13:15:59 +0530

    typescript task demo page

---
+++ TypeScript Task - Demo/main.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let name;
+name = 'Kanagaraj';
+name = 'K';
+console.log('Hello ' + name);
+let age;
+age = 30;
+let isElgibibleToVote;
+isElgibibleToVote = age > 18;
+const sum = (num1, num2) => {
+    return num1 + num2;
+};
+console.log(sum(10, 20));
+//# sourceMappingURL=main.js.map
---
+++ TypeScript Task - Demo/main.ts
@@ -0,0 +1,22 @@
+let name: String;
+
+name = 'Kanagaraj'
+name = 'K'
+
+console.log('Hello ' + name);
+
+
+let age: number;
+
+age = 30;
+let isElgibibleToVote: boolean;
+
+isElgibibleToVote = age > 18
+
+
+
+const sum = (num1: number, num2: number): number => {
+    return num1 + num2
+}
+
+console.log(sum(10, 20))
